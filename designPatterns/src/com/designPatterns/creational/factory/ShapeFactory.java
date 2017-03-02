bdcb1a20be4cff86b90897f7d450ae84c6d50f15
Roberto Leandro <rleandro.dev@gmail.com>
2017-03-01 22:35:06 -0300

    firt commit

---
+++ designPatterns/src/com/designPatterns/creational/factory/ShapeFactory.java
@@ -0,0 +1,24 @@
+package com.designPatterns.creational.factory;
+
+public class ShapeFactory {
+
+	public Shape getShape(String shapeType){
+		if(shapeType == null){
+			return null;
+		}
+		
+		if(shapeType.equalsIgnoreCase("Circle")){
+			return new Circle();
+		}
+		
+		if(shapeType.equalsIgnoreCase("Rectangle")){
+			return new Rectangle();
+		}
+		
+		if(shapeType.equalsIgnoreCase("Square")){
+			return new Square();
+		}
+		
+		return null;
+	}
+}
